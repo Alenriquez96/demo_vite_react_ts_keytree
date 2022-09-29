2d7f5cb802eaf63026c1429458634c5b8ea5747c
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-09-29 12:16:46 +0200

    add/themeIcons

--- src/components/Header/ThemeButton/ThemeButton.tsx
+++ src/components/Header/ThemeButton/ThemeButton.tsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import { themeContext } from '../../../context/themeContext';
+import { MoonIcon,SunIcon } from '@chakra-ui/icons'
 
 const ThemeButton = () => {
     const {theme, toggleTheme } = useContext(themeContext);
 
   return (
-    <button onClick={toggleTheme}>ThemeButton</button>
+    <button onClick={toggleTheme}>{theme===""?<MoonIcon/>:<SunIcon/>}</button>
   )
 }
 
