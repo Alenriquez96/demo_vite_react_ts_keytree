368d0548935c562e93934ca03bc03d457eebdfc9
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-09-29 13:56:34 +0200

    add/searchBar

--- src/components/Main/SearchBar/SearchBar.tsx
+++ src/components/Main/SearchBar/SearchBar.tsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
+import lupa from "../../../assets/lupa.png";
+import eliminar from "../../../assets/eliminar.png";
 
 
-const SearchBar = () => {
+const SearchBar:()=>JSX.Element = () => {
     const [book, setBook] = useState("");
+    const [text, setText] = useState("");        
+    
 
-    const handleSubmit = (e:any) =>{
+    const handleSubmit:(e:any)=>void = (e) =>{
         e.preventDefault();
         setBook(e.target.book.value);
     }    
 
+    const handleText:()=>void =()=>{
+      let inpField:any = document.querySelector("#searchBar > input[type=text]");
+       setText("");
+       inpField!.value="";
+    }
+
   return (
-    <form onSubmit={handleSubmit}>
-        <input name='book' type="text" placeholder='Type title, author, genre, editor, publisher...'/>
+    <form  onSubmit={handleSubmit}id='searchBar'>
+        <button><img src={lupa} alt="lupa" /></button>
+        <input onChange={(e)=>{
+          setText(e.target.value)
+          }} name='book' type="text" placeholder='Type title, author, genre, editor, publisher...'/>
+        {text!==""?<button onClick={handleText}><img src={eliminar} alt="eliminar texto" /></button>:""}
     </form>
   )
 }
